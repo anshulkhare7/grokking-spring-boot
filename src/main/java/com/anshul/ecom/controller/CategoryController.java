a5dc06f267c4e5c1632af352d4536cf1e37ac49a
Anshul <anshulkhare7@gmail.com>
2024-09-26 09:34:54 +0530

    Response Entity

--- src/main/java/com/anshul/ecom/controller/CategoryController.java
+++ src/main/java/com/anshul/ecom/controller/CategoryController.java
@@ -6,14 +6,28 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.anshul.ecom.model.Category;
+import com.anshul.ecom.service.CategoryService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
 public class CategoryController {
 
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping("/categories")    
     public List<Category> getAllCategories() {
-        return new ArrayList<Category>();
+        return categoryService.getAllCategories();
+    }
+
+    @PostMapping("/category")    
+    public String createCategory(@RequestBody Category category) {
+        return categoryService.createCategory(category);
     }
 }
--- src/main/java/com/anshul/ecom/controller/GreetingsController.java
+++ src/main/java/com/anshul/ecom/controller/GreetingsController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.anshul.ecom.request.Body;
 import com.anshul.ecom.response.Response;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,13 +17,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class GreetingsController {
     
     @GetMapping("/hello")    
-    public Response hello() {
-        return new Response("Hello World!");
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<>("Hello World!",HttpStatus.OK);
     }
 
     @GetMapping("/hellowithparam/{name}")    
-    public Response helloWithParam(@PathVariable String name) {
-        return new Response("Hello, "+name);
+    public ResponseEntity<String> helloWithParam(@PathVariable String name) {
+        return new ResponseEntity<>("Hello, " + name, HttpStatus.OK);        
     }
 
     @PostMapping("/bye")
