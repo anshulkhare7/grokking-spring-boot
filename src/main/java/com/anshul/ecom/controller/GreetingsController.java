49165297e26c0141d2eff9fb3f967791e07b5830
Anshul <anshulkhare7@gmail.com>
2024-09-26 10:00:53 +0530

    jpa. jdk 22 -> 17. fixed the controller to pass the test

--- src/main/java/com/anshul/ecom/controller/GreetingsController.java
+++ src/main/java/com/anshul/ecom/controller/GreetingsController.java
@@ -1,33 +1,32 @@
 package com.anshul.ecom.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import com.anshul.ecom.request.Body;
-import com.anshul.ecom.response.Response;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.anshul.ecom.request.Body;
+import com.anshul.ecom.response.Response;
 
 
 @RestController
 public class GreetingsController {
     
     @GetMapping("/hello")    
-    public ResponseEntity<String> hello() {
-        return new ResponseEntity<>("Hello World!",HttpStatus.OK);
+    public ResponseEntity<Response> hello() {
+        return new ResponseEntity<>(new Response("Hello World!"), HttpStatus.OK);
     }
 
     @GetMapping("/hellowithparam/{name}")    
-    public ResponseEntity<String> helloWithParam(@PathVariable String name) {
-        return new ResponseEntity<>("Hello, " + name, HttpStatus.OK);        
+    public ResponseEntity<Response> helloWithParam(@PathVariable String name) {
+        return new ResponseEntity<>(new Response("Hello, " + name), HttpStatus.OK);        
     }
 
     @PostMapping("/bye")
-    public Response bye(@RequestBody Body reqBody) {
-        return new Response("Bye! "+reqBody.getMesg());
+    public ResponseEntity<Response> bye(@RequestBody Body reqBody) {
+        return new ResponseEntity<>(new Response("Bye! " + reqBody.getMesg()), HttpStatus.OK);
     }
 }
