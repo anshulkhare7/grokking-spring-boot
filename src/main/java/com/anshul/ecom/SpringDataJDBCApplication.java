86f66365e8c24d69618bc1b50c7ac25f01909f9d
Anshul <anshulkhare7@gmail.com>
2024-09-28 14:11:38 +0530

    CRUD with JDBCRepository

--- src/main/java/com/anshul/ecom/SpringDataJDBCApplication.java
+++ src/main/java/com/anshul/ecom/SpringDataJDBCApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import com.anshul.ecom.entity.Author;
+import com.anshul.ecom.entity.Book;
 import com.anshul.ecom.repository.AuthorRepository;
 import com.anshul.ecom.repository.BookRepository;
 
@@ -23,20 +25,46 @@ public class SpringDataJDBCApplication implements CommandLineRunner {
 		SpringApplication.run(SpringDataJDBCApplication.class, args);
 	}
 
-	@Autowired
+    @Autowired
     BookRepository bookRepository;
 
     @Autowired
     AuthorRepository authorRepository;
 
-    @Override
+	@Override
     public void run(String... strings) throws Exception {
-        log.info("find all books");
+        log.info("CRUD features started");
 
-        log.info(String.valueOf(bookRepository.findAll()));
+        log.info("CREATE");
 
-        log.info("find all authors");
+        Book egoIsTheEnemy = new Book();
+        egoIsTheEnemy.setName("Ego is the enemy");
+        egoIsTheEnemy.setSummary("The fight to master our greatest opponent");
+        bookRepository.save(egoIsTheEnemy);
+        
+        Author ryanHoliday = new Author();
+        ryanHoliday.setFirstName("Ryan");
+        ryanHoliday.setLastName("Holiday");
+        authorRepository.save(ryanHoliday);
 
-        log.info(String.valueOf(authorRepository.findAll()));
+        log.info("READ");
+        
+        log.info(String.valueOf(bookRepository.findById(egoIsTheEnemy.getId())));
+        log.info(String.valueOf(authorRepository.findById(ryanHoliday.getId())));
+
+        log.info("UDPATE");
+        
+        egoIsTheEnemy.setSummary("The fight to master our greatest opponent from Ryan Holiday");
+        bookRepository.save(egoIsTheEnemy);
+
+        ryanHoliday.setLastName("Holi");
+        authorRepository.save(ryanHoliday);
+
+        log.info("DELETE");
+        
+        bookRepository.delete(egoIsTheEnemy);
+        authorRepository.delete(ryanHoliday);
+
+        log.info("CRUD features done");
     }
 }
