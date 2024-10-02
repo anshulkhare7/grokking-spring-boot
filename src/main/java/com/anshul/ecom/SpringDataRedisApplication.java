11f80b989b405659398f92adfbd51781a4c20dad
Anshul <anshulkhare7@gmail.com>
2024-10-02 11:37:34 +0530

    Redis Pub Sub

--- src/main/java/com/anshul/ecom/SpringDataRedisApplication.java
+++ src/main/java/com/anshul/ecom/SpringDataRedisApplication.java
@@ -1,19 +1,18 @@
 package com.anshul.ecom;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.redis.core.HashOperations;
-import org.springframework.data.redis.core.ListOperations;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.SetOperations;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+import com.anshul.ecom.service.RedisMessagePubSub;
+
 
 @SpringBootApplication
 public class SpringDataRedisApplication implements CommandLineRunner {
@@ -27,70 +26,20 @@ public class SpringDataRedisApplication implements CommandLineRunner {
     @Autowired
     private RedisTemplate redisTemplate;
 
+    @Autowired
+    private RedisMessagePubSub publisherSubscriber;
+
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer() {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisTemplate.getConnectionFactory());
+        container.addMessageListener(publisherSubscriber, new ChannelTopic("educative:queue"));
+        return container;
+    }
+
     @Override
     public void run(String... args) throws Exception {
-        //set the String serializer for key and value
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-
-        //string operations
-        redisTemplate.opsForValue().set("Book", "Author");
-
-        log.info(String.valueOf(redisTemplate.hasKey("Book")));
-        log.info(String.valueOf(redisTemplate.opsForValue().get("Book")));
-
-        //list operator
-        ListOperations redisListOperator = redisTemplate.opsForList();
-
-        //list operations
-        redisListOperator.rightPush("BookList", "Atomic Habits");
-        redisListOperator.rightPush("BookList", "Martian");
-        redisListOperator.rightPush("BookList", "The Psychology Of Money");
-        redisListOperator.rightPush("BookList", "Zero To One");
-
-        log.info(String.valueOf(redisTemplate.hasKey("BookList")));
-        log.info(String.valueOf(redisListOperator.size("BookList")));
-        log.info(String.valueOf(redisListOperator.index("BookList", 1)));
-        log.info(String.valueOf(redisListOperator.range("BookList", 2, 3)));
-
-        redisListOperator.set("BookList", 1, "Project Hail Mary");
-        log.info(String.valueOf(redisListOperator.index("BookList", 1)));
-
-        //set operator
-        SetOperations redisSetOperator = redisTemplate.opsForSet();
-
-        //set operations
-        redisSetOperator.add("BookSet", "Atomic Habits", "Martian", "The Psychology Of Money", "Leaders eat last");
-
-        log.info(String.valueOf(redisSetOperator.size("BookSet")));
-
-        log.info(String.valueOf(redisSetOperator.isMember("BookSet", "Martian")));
-        log.info(String.valueOf(redisSetOperator.isMember("BookSet", "Zero To One")));
-
-        log.info(redisSetOperator.members("BookSet").toString());
-
-        redisSetOperator.remove("BookSet", "Martian");
-
-        log.info(redisSetOperator.members("BookSet").toString());
-
-        //set the String serializer for Hash key and value
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
-
-        //Hash operator
-        HashOperations redisHashOperator = redisTemplate.opsForHash();
-
-        //Hash operations
-        Map thePsychologyOfMoney = new HashMap<String, String>();
-        thePsychologyOfMoney.put("id", "thePsychologyOfMoney");
-        thePsychologyOfMoney.put("name", "The Psychology of Money");
-        thePsychologyOfMoney.put("summary", "Timeless Lessons on Wealth, Greed, and Happiness");
-
-        redisHashOperator.putAll("Book" + thePsychologyOfMoney.get("id"), thePsychologyOfMoney);
-
-        log.info(String.valueOf(redisHashOperator.entries("Book" + thePsychologyOfMoney.get("id"))));
-
-        // HashMapper<Book, String, String> bookHashMapper = new DecoratingStringHashMapper<>(new PojoHashMapper<>(Book.class));
-        // log.info(String.valueOf(bookHashMapper.fromHash(redisHashOperator.entries("Book" + thePsychologyOfMoney.get("id")))));
+        //Redis messaging
+        publisherSubscriber.publishMessage("educative:queue", "hello there");
     }
 }
